658e2e7372e71d2cc1658aa166eddcde1cbaba36
pudasainianjan <pudasainianjan@gmail.com>
2020-04-03 21:20:07 +0545

    Updates

---
+++ precache-manifest.5a9726e156bed16523fedda8df8d18a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39b3f9f5d42ddccfe691ccced570da57",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "170876b92fda42bef18a",
+    "url": "/smart-brain/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "6891cb636c3dc49591df",
+    "url": "/smart-brain/static/css/main.1eb1a45c.chunk.css"
+  },
+  {
+    "revision": "170876b92fda42bef18a",
+    "url": "/smart-brain/static/js/2.66fe6cec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-brain/static/js/2.66fe6cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6891cb636c3dc49591df",
+    "url": "/smart-brain/static/js/main.4901ec0f.chunk.js"
+  },
+  {
+    "revision": "fbe09a41306805dc0bed",
+    "url": "/smart-brain/static/js/runtime-main.8b28d6d8.js"
+  }
+]);
